a9bb9923b7c7c785365ec3271141fc272e46023f
Radouane Iben-hamou <reben-ha@student.1337.ma>
2023-08-27 16:30:27 +0100

    #

--- rendering/render.c
+++ rendering/render.c
@@ -6,7 +6,7 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 20:29:43 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/08/27 16:01:16 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/08/27 16:30:22 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ void	to_3d_ray(mlx_image_t *image, double wp, int i)
 	t_pos	pos;
 	int		end_y;
 
+	if (wp > HEIGHT)
+		wp = HEIGHT;
 	pos.x = i;
 	pos.y = (HEIGHT / 2) - (wp / 2);
 	end_y = (HEIGHT / 2) + (wp / 2);
@@ -36,7 +38,7 @@ void	to_3d_ray(mlx_image_t *image, double wp, int i)
 	{
 		mlx_put_pixel_p(image
 			, pos.x, pos.y
-			, get_rgb(12, 53, 106, wp)); // chnage this
+			, get_rgb(12, 53, 106, 255));
 		pos.y++;
 	}
 }
