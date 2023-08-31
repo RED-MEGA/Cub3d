908613f1cf5c068564f5aaf0f3f7445c59b6b4c3
Radouane Iben-hamou <reben-ha@student.1337.ma>
2023-08-31 16:24:27 +0100

    l3abd get_next_line

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -5,60 +5,105 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/21 15:32:54 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/08/01 19:53:38 by reben-ha         ###   ########.fr       */
+/*   Created: 2023/08/31 15:42:42 by reben-ha          #+#    #+#             */
+/*   Updated: 2023/08/31 15:42:46 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_read_join(int fd, char *save_area, int *fail)
+static int	ft_check_slash(char *s)
 {
-	char		*buffer;
+	int	i;
 
-	buffer = (char *)malloc(BUFFER_SIZE * sizeof(char) + 1);
-	ft_error_ptr(buffer, 1);
-	while ((*fail) > 0 && ft_strchr_cheack_gnl(save_area, '\n') == 0)
+	i = 0;
+	while (s[i])
 	{
-		(*fail) = read(fd, buffer, BUFFER_SIZE);
-		if ((*fail) == -1)
-		{
-			free(save_area);
-			save_area = NULL;
-			return (free(buffer), buffer = NULL);
-		}
-		buffer[(*fail)] = '\0';
-		save_area = ft_strjoin_gnl(save_area, buffer);
+		if (s[i] == '\n')
+			return (0);
+		i++;
 	}
-	free(buffer);
-	return (save_area);
+	return (1);
+}
+
+static char	*ft_line(char *stv)
+{
+	char	*c;
+	int		i;
+
+	i = 0;
+	if (!stv[i])
+		return (NULL);
+	if (stv[i] != '\n' && stv[i])
+		i = ft_strlen(stv);
+	if (ft_check_slash(stv))
+		c = malloc(i + 1);
+	else
+		c = malloc(i + 2);
+	if (!c)
+		return (NULL);
+	i = 0;
+	while (stv[i] != '\n' && stv[i])
+	{
+		c[i] = stv[i];
+		i++;
+	}
+	if (!ft_check_slash(stv))
+		c[i++] = '\n';
+	c[i] = '\0';
+	return (c);
+}
+
+static char	*ft_next(char *stv)
+{
+	int		j;
+	int		i;
+	char	*st;
+
+	j = 0;
+	i = 0;
+	while (stv[j] != '\n' && stv[j])
+		j++;
+	if (!ft_check_slash(stv))
+		j++;
+	if (ft_strlen(stv) == j)
+		return (free(stv), NULL);
+	st = malloc(ft_strlen(stv) - j + 1);
+	if (!st)
+		return (NULL);
+	while (stv[j])
+		st[i++] = stv[j++];
+	st[i] = '\0';
+	free(stv);
+	stv = NULL;
+	return (st);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*save_area;
-	char		*result;
-	char		*temp;
-	int			fail;
+	int			i;
+	char		*buff;
+	static char	*stv;
+	char		*c;
 
-	if (fd == -1 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	fail = 1;
-	save_area = ft_read_join(fd, save_area, &fail);
-	if (fail == 0 && save_area[0] == '\0')
-		return (free(save_area), save_area = NULL);
-	else if (fail == 0)
+	if (!stv)
+		stv = ft_strdup("");
+	i = 1;
+	buff = malloc(BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	while (ft_check_slash(stv) && i)
 	{
-		result = ft_strchr_gnl(save_area, '\n', 3);
-		free(save_area);
-		save_area = NULL;
+		i = read(fd, buff, BUFFER_SIZE);
+		if (i < 0)
+			return (free(buff), free(stv), stv = NULL, NULL);
+		buff[i] = '\0';
+		stv = ft_strjoin(stv, buff);
 	}
-	else
-	{
-		result = ft_strchr_gnl(save_area, '\n', 1);
-		temp = save_area;
-		save_area = ft_strchr_gnl(save_area, '\n', 2);
-		free(temp);
-	}
-	return (result);
+	free(buff);
+	c = ft_line(stv);
+	stv = ft_next(stv);
+	return (c);
 }
