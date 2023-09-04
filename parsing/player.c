2ec613e6f5b06558e16df05a5fe494919e411f83
Radouane Iben-hamou <reben-ha@student.1337.ma>
2023-09-04 14:07:32 +0100

    #

--- parsing/player.c
+++ parsing/player.c
@@ -6,23 +6,23 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 13:56:34 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/09/04 13:58:09 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/09/04 14:04:49 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	_set_pi(char **map, t_player *player, char c, t_loc pos)
+void	_set_pi(char **map, t_player *player, t_loc pos)
 {
 	player->pos.x = (pos.j * SQUARE_LEN) + (SQUARE_LEN / 2);
 	player->pos.y = (pos.i * SQUARE_LEN) + (SQUARE_LEN / 2);
-	if (c == 'N')
+	if (player->key == 'N')
 		player->direction = NO;
-	else if (c == 'S')
+	else if (player->key == 'S')
 		player->direction = SO;
-	else if (c == 'W')
+	else if (player->key == 'W')
 		player->direction = WE;
-	else if (c == 'E')
+	else if (player->key == 'E')
 		player->direction = EA;
 }
 
@@ -39,9 +39,7 @@ void	init_player(t_info *info)
 		{
 			if (info->map[loc.i][loc.j] == 'P')
 			{
-				_set_pi(info->map, &info->player 
-						, info->player.key
-						, loc);
+				_set_pi(info->map, &info->player, loc);
 				return ;
 			}
 		}
