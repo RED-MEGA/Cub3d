c1398c6c458077cefe9329bd7a90e82b7094e9f8
Radouane Iben-hamou <reben-ha@student.1337.ma>
2023-09-09 17:58:04 +0100

    #

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 23:50:11 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/09/09 17:53:00 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/09/09 17:57:25 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,10 +50,6 @@ void	cub3d(char *file_name)
 
 	info = parsing(file_name);
 	pub = init_global(info);
-
-	//---------------------------------------------------------------------------
-
-	// mlx = pub->mlx;
 	mlx_mouse_hook(pub->mlx, handle_mouse_hook, pub);
 	mlx_key_hook(pub->mlx, handle_keys, pub);
 	mlx_cursor_hook(pub->mlx, handle_mouse, pub);
@@ -61,11 +57,6 @@ void	cub3d(char *file_name)
 	mlx_loop_hook(pub->mlx, refresh_frame, pub);
 	mlx_loop(pub->mlx);
 	mlx_terminate(pub->mlx);
-	destroy_global(pub);
-	// use this for mlx resources
-	// mlx_delete_xpm42();
-	// mlx_delete_image();
-	// mlx_delete_texture();
 }
 
 int	main(int argc, char **argv)
@@ -75,24 +66,3 @@ int	main(int argc, char **argv)
 	cub3d(argv[1]);
 	return (0);
 }
-
-
-
-
-
-// Parsing debug
-// int debug_fd = open("Debug.log", O_RDWR);
-// dprintf(debug_fd, "/* **************** */\n");
-// dprintf(debug_fd, "/*     PARSING      */\n");
-// dprintf(debug_fd, "/* **************** */\n");
-// dprintf(debug_fd, "\nAttribute : \n");
-// dprintf(debug_fd, "NO : %s \n", info->NO);
-// dprintf(debug_fd, "SO : %s \n", info->SO);
-// dprintf(debug_fd, "WE : %s \n", info->WE);
-// dprintf(debug_fd, "EA : %s \n", info->EA);
-// dprintf(debug_fd, "F : (%d,%d,%d) \n", info->F.r, info->F.g, info->F.b);
-// dprintf(debug_fd, "C : (%d,%d,%d) \n", info->C.r, info->C.g, info->C.b);
-// dprintf(debug_fd, "\nMap :\n");
-// for (size_t i = 0; info->map[i]; i++)
-// 	dprintf(debug_fd, "%s:newline\n", info->map[i]);
-// printf("%sSUCCESS\n", GREEN);
